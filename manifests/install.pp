bc675384d65868be39d0b30b517a501ae0ddeaef
Sander Hoentjen <shoentjen@antagonist.nl>
2017-05-24 14:44:54 +0200

    add root_dir to prevent redownloading every run

--- manifests/install.pp
+++ manifests/install.pp
@@ -7,6 +7,7 @@ class teamcity::install inherits teamcity::params  {
   $teamcity_version               = $teamcity::params::teamcity_version
   $teamcity_base_url              = $teamcity::params::teamcity_base_url
   $teamcity_download_timeout      = $teamcity::params::teamcity_download_timeout
+  $teamcity_root_dir              = $teamcity::params::teamcity_root_dir
 
   $db_type                        = $teamcity::params::db_type
   $jdbc_download_url              = $teamcity::params::jdbc_download_url
@@ -43,24 +44,24 @@ class teamcity::install inherits teamcity::params  {
     group   => 'teamcity',
   } ->
 
-  file { "/opt/teamcity":
-    ensure  => 'link',
-    target  => "/opt/teamcity-${teamcity_version}/TeamCity",
-    before  => Archive["teamcity-${teamcity_version}"],
+  file { '/opt/teamcity':
+    ensure => 'link',
+    target => "/opt/teamcity-${teamcity_version}/${teamcity_root_dir}",
+    before => Archive["teamcity-${teamcity_version}"],
   }
 
   if $::teamcity::params::archive_provider == 'camptocamp' {
     archive { "teamcity-${teamcity_version}":
-      ensure            => 'present',
-      url               => $use_download_url,
-      target            => $use_target_dir,
-      src_target        => '/opt/teamcity-sources',
-      follow_redirects  => true,
-      checksum          => false,
-      strip_components  => 1,
-      user              => 'teamcity',
-      before            => File[$teamcity_data_path],
-      timeout           => $teamcity_download_timeout,
+      ensure           => 'present',
+      url              => $use_download_url,
+      target           => $use_target_dir,
+      root_dir         => $teamcity_root_dir,
+      src_target       => '/opt/teamcity-sources',
+      follow_redirects => true,
+      checksum         => false,
+      user             => 'teamcity',
+      before           => File[$teamcity_data_path],
+      timeout          => $teamcity_download_timeout,
     }
   } else {
     # must be puppet :)
@@ -70,7 +71,7 @@ class teamcity::install inherits teamcity::params  {
       extract         => true,
       source          => $use_download_url,
       extract_path    => $use_target_dir,
-      creates         => "${use_target_dir}/TeamCity/bin",
+      creates         => "${use_target_dir}/${teamcity_root_dir}",
       checksum_verify => false,
       cleanup         => true,
       before          => File[$teamcity_data_path],
